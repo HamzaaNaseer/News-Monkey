bde8c174f5e01b16578c9d1bddf81ca66c2d5ece
HamzaaNaseer <hamzanaseer711@gmail.com>
2021-11-02 13:47:04 +0500

    implemented loading spinner and few styling changes

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 
 export default class News extends Component {
   constructor() {
@@ -13,37 +14,43 @@ export default class News extends Component {
     };
   }
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     const response = await fetch(url);
     const parsedResponse = await response.json();
 
     this.setState({
       articles: parsedResponse.articles,
       totalResults: parsedResponse.totalResults,
+      loading: false,
     });
   }
   prevHandler = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304&page=${
-      this.state.page - 1
-    }`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304&pageSize=${
+      this.props.pageSize
+    }&page=${this.state.page - 1}`;
+    this.setState({ loading: true });
     const response = await fetch(url);
     const parsedResponse = await response.json();
 
     this.setState({
       articles: parsedResponse.articles,
       page: this.state.page - 1,
+      loading: false,
     });
   };
   nextHandler = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304&page=${
-      this.state.page + 1
-    }`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=421dc721667c40f2a7a0b935d0a1d304&pageSize=${
+      this.props.pageSize
+    }&page=${this.state.page + 1}`;
+    this.setState({ loading: true });
     const response = await fetch(url);
     const parsedResponse = await response.json();
 
     this.setState({
       articles: parsedResponse.articles,
       page: this.state.page + 1,
+      loading: false,
     });
   };
 
@@ -51,20 +58,24 @@ export default class News extends Component {
     console.log("rendered called");
     return (
       <div className="container my-3">
-        <h2>DailyNewsDose || Top Headlines</h2>
+        <h2 className="text-center">DailyNewsDose || Top Headlines</h2>
+        {/* loading spinner will show only when the loading state is true otherwise none */}
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <Newsitem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage}
-                  url={element.url}
-                />
-              </div>
-            );
-          })}
+          {/* news articles will only be shown when the spinner is false otherwise none  */}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <Newsitem
+                    title={element.title}
+                    description={element.description}
+                    imageUrl={element.urlToImage}
+                    url={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -80,7 +91,8 @@ export default class News extends Component {
             type="button"
             className="btn btn-dark"
             disabled={
-              Math.ceil(this.state.totalResults / 20) <= this.state.page
+              this.state.page >=
+              Math.ceil(this.state.totalResults / this.props.pageSize)
             }
           >
             next &#8594;
