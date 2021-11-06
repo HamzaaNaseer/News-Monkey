a422b8e8883a2c74906ce787dac8d458d729921d
HamzaaNaseer <hamzanaseer711@gmail.com>
2021-11-06 14:07:12 +0500

    added infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -20,6 +21,7 @@ export default class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
     };
   }
   async componentDidMount() {
@@ -34,75 +36,62 @@ export default class News extends Component {
       loading: false,
     });
   }
-  pageHandler = async (type) => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${
-      this.props.category
-    }&apiKey=421dc721667c40f2a7a0b935d0a1d304&pageSize=${
-      this.props.pageSize
-    }&page=${type === "next" ? this.state.page + 1 : this.state.page - 1}`;
-    this.setState({ loading: true });
+  pageHandler = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category
+      }&apiKey=421dc721667c40f2a7a0b935d0a1d304&pageSize=${this.props.pageSize
+      }&page=${this.state.page + 1}`;
     const response = await fetch(url);
     const parsedResponse = await response.json();
 
     this.setState({
-      articles: parsedResponse.articles,
-      page: type === "next" ? this.state.page + 1 : this.state.page - 1,
-      loading: false,
+      articles: this.state.articles.concat(parsedResponse.articles),
+      page: this.state.page + 1,
+      totalResults: parsedResponse.totalResults,
     });
+  };
+  fetchMoreData = async () => {
+
+    this.pageHandler('next')
+
+
   };
 
   render() {
     console.log("rendered called");
     return (
-      <div className="container my-3">
+      <>
         <h2 className="text-center">DailyNewsDose || Top Headlines</h2>
         {/* loading spinner will show only when the loading state is true otherwise none */}
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {/* news articles will only be shown when the spinner is false otherwise none  */}
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <Newsitem
-                    title={element.title}
-                    description={element.description}
-                    imageUrl={element.urlToImage}
-                    url={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            onClick={() => {
-              this.pageHandler("prev");
-            }}
-            type="button"
-            className="btn btn-dark"
-            disabled={this.state.page <= 1}
-          >
-            &#8592; prev
-          </button>
-          <button
-            onClick={() => {
-              this.pageHandler("next");
-            }}
-            type="button"
-            className="btn btn-dark"
-            disabled={
-              this.state.page >=
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-          >
-            next &#8594;
-          </button>
-        </div>
-      </div>
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className='container'>
+            <div className="row">
+              {/* news articles will only be shown when the spinner is false otherwise none  */}
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <Newsitem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={element.urlToImage}
+                      url={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
     );
   }
 }
